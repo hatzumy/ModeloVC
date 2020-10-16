a5288a1fd53be58787cc2960816af0ca6ce44ba6
hatzumy <tellezmaleja@gmail.com>
2020-10-16 05:32:08 -0500

    se añade las carpetas MVC y se agrega la clase Empleado

---
+++ MVC/src/Modelo/Empleado.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Mary Alejandra
+ */
+public class Empleado {
+    String cedula ;
+    String contraseña;
+    String apellidos;
+    String nombres;
+    
+    public Empleado(){
+        cedula="";
+        contraseña="";
+        apellidos="";
+        nombres="";
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+}
