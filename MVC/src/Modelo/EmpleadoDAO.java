1ef22a5157be40413b0510528fc97f1d1fac8488
hatzumy <tellezmaleja@gmail.com>
2020-10-16 05:59:52 -0500

    se añade la clase empleadoDAO y la vista login

---
+++ MVC/src/Modelo/EmpleadoDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Mary Alejandra
+ */
+public class EmpleadoDAO {
+    
+   
+    public Empleado verificaUsuario(String cedula, String contraseña){
+        Empleado empleado=null;
+        String cc = "123456789";
+        String password = "12345";
+        String apellidos = "Perez";
+        String nombres = "Pedro";
+        try {
+            empleado = new Empleado();
+            
+            if(cedula.equals(cc)&& contraseña.equals(password)){
+               empleado = new Empleado();
+               empleado.setCedula(cc);
+               empleado.setContraseña(password);
+               empleado.setApellidos(apellidos);
+               empleado.setNombres(nombres);
+               return empleado;
+            }
+        } catch (Exception e) {
+        }
+        return empleado;
+    }
+}
